fdf99719997ef93c97347b6cba0167c3949f0f7d
Jim Winquist <jimwinquist@gatech.edu>
2017-02-03 16:53:54 -0800

    feat: Add stubbed in code for extracting color and histogram features

--- vehicle_detection.py
+++ vehicle_detection.py
@@ -1,7 +1,14 @@
+import glob
+import time
+
 import cv2
 import numpy as np
 import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
 from skimage.feature import hog
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 def draw_boxes(img, bboxes, color=(0, 0, 255), thickness=6):
     '''
@@ -59,3 +66,77 @@ def get_hog_features(img, orient, pix_per_cell, cell_per_block, vis=False, featu
                        cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=False,
                        visualise=False, feature_vector=feature_vec)
         return features
+
+def extract_features(imgs, cspace='RGB', spatial_size=(32, 32),
+                        hist_bins=32, hist_range=(0, 256)):
+    '''
+    Extract features from a list of images
+
+    :param imgs:
+    :param cspace:
+    :param spatial_size:
+    :param hist_bins:
+    :param hist_range:
+    :return:
+    '''
+    features = []
+    for file in imgs:
+        image = mpimg.imread(file)
+        if cspace != 'RGB':
+            if cspace == 'HSV':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+            elif cspace == 'LUV':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2LUV)
+            elif cspace == 'HLS':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+            elif cspace == 'YUV':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
+        else: feature_image = np.copy(image)
+        spatial_features = bin_spatial(feature_image, size=spatial_size)
+        hist_features = color_hist(feature_image, nbins=hist_bins, bins_range=hist_range)
+        features.append(np.concatenate((spatial_features, hist_features)))
+    return features
+
+def bin_spatial(img, color_space='RGB', size=(32, 32)):
+    '''
+    Compute color histogram features.
+
+    Pass the color_space flag as 3-letter all caps string like 'HSV' or 'LUV' etc.
+
+    :param img:
+    :param color_space:
+    :param size:
+    :return:
+    '''
+    if color_space != 'RGB':
+        if color_space == 'HSV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+        elif color_space == 'LUV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
+        elif color_space == 'HLS':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+        elif color_space == 'YUV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+        elif color_space == 'YCrCb':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
+    else: feature_image = np.copy(img)
+    features = cv2.resize(feature_image, size).ravel()
+    return features
+
+def color_hist(img, nbins=32, bins_range=(0, 256)):
+    '''
+    Compute color histogram features
+
+    :param img:
+    :param nbins:
+    :param bins_range:
+    :return:
+    '''
+    channel1_hist = np.histogram(img[:,:,0], bins=nbins, range=bins_range)
+    channel2_hist = np.histogram(img[:,:,1], bins=nbins, range=bins_range)
+    channel3_hist = np.histogram(img[:,:,2], bins=nbins, range=bins_range)
+
+    bin_edges = channel1_hist[1]
+    bin_centers = (bin_edges[1:]  + bin_edges[0:len(bin_edges)-1])/2
+    hist_features = np.concatenate((channel1_hist[0], channel2_hist[0], channel3_hist[0]))
+    return channel1_hist, channel2_hist, channel3_hist, bin_centers, hist_features
